fe7a48517c72e6c835e070b6f04201dbc6ee2a04
Yuvraj Singh Solanki <111236237+yuvraj-solanki-2406@users.noreply.github.com>
2024-02-17 14:34:38 +0530

    Update TransactionList.vue

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,7 +2,7 @@
   <h3>History</h3>
   <ul id="list" class="list">
     <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
-      {{ transaction.text }} <span>₹{{ transaction.amount }}</span>
+      {{ transaction.text }} <span> Rs{{ transaction.amount }}</span>
       <button @click="deleteTransaction(transaction.id)" 
       class="delete-btn">x</button>
     </li>
